5d188c612fab3c7d976b6f9103552d36065f5aa3
leyEduardo <leyeduardo@leyeduardo.lan>
2025-04-06 20:48:13 -0300

    estudando git e github

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
  olá esse projeto ensina  o git
+ isso é uma alteração
+ 
--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print(" Hello word")
 
 print(" estou começando estudando python agora")
+
+print(" fiz uma alteração para treinar o git e github")
